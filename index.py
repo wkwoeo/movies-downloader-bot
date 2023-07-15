525f429002355197c08b5d03c09e2bd11493ed96
Mohammed alhamdi <104478279+wkwoeo@users.noreply.github.com>
2023-07-15 16:10:39 +0300

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = os.getenv("https://movies-downloader-riqg6lyiv-wkwoeo.vercel.app")
+URL = os.getenv("https://movies-downloader-riqg6lyiv-wkwoeo.vercel.app/")
 bot = Bot(TOKEN)
 
 
